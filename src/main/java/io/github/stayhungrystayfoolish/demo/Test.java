4b5f710e4791ecc5664cc27f856eca7d29091a2c
StayHungryStayFoolish <bonismo@hotmail.com>
2018-08-14 13:20:49 +0800

    test

---
+++ src/main/java/io/github/stayhungrystayfoolish/demo/Test.java
@@ -0,0 +1,13 @@
+package io.github.stayhungrystayfoolish.demo;
+
+/**
+ * Created by bonismo@hotmail.com on 2018/8/14 下午1:20
+ *
+ * @Version: V1.0.0
+ * <p>
+ * Description:
+ */
+public class Test {
+}
+
+
